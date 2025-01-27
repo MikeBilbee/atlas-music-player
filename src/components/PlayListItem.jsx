7ba7106faa284d8e62604eccba8c4e47c43229ac
MikeBilbee <cmbilbee@gmail.com>
2025-01-27 12:22:23 -0600

    2.0 adding components

---
+++ src/components/PlayListItem.jsx
@@ -0,0 +1,17 @@
+// PlayListItem.jsx
+import React from 'react';
+
+const PlayListItem = ({ title, artist, length }) => {
+	
+	return (
+		<div className="flex items-center justify-between py-1 px-5">
+			<div>
+				<h3 className="text-lg font-medium dark:text-gray-400">{title}</h3>
+				<p className="dark:text-gray-500 text-sm">{artist}</p>
+			</div>
+			<p className="dark:text-gray-500 text-sm">{length}</p>
+		</div>
+	);
+};
+
+export default PlayListItem;
