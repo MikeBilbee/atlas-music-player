14292e91c633a10d83264a2041d0ff5d34659e5c
MikeBilbee <cmbilbee@gmail.com>
2025-01-28 14:57:29 -0600

    7.1 skeleton update

--- src/components/LoadingSkeleton.jsx
+++ src/components/LoadingSkeleton.jsx
@@ -1,10 +1,10 @@
 //LoadingSkeleton.jsx
 const LoadingSkeleton = () => {
 	return (
-	  <div className='flex flex-col md:flex-row md:space-x-4 md:space-y-0 space-y-4 w-full max-w-5xl mx-auto p-6 bg-brightYellow dark:bg-black text-darkMagenta dark:text-white rounded-lg shadow-2xl transition-all duration-300 animate-pulse'>
+	  <div className='flex flex-col md:flex-row md:space-x-4 md:space-y-0 space-y-4 w-full max-w-5xl mx-auto p-6 dark:bg-black text-darkMagenta dark:text-white rounded-lg shadow-2xl transition-all duration-300 animate-pulse'>
 
-		<section className='md:w-1/2 bg-brightPink border-teal border-4 rounded-xl shadow-lg p-6 dark:bg-darkMagenta dark:border-white animate-pulse'>
-		  <div className='p-6 bg-brightYellow dark:bg-darkerTeal rounded-lg shadow-lg border-4 border-magenta transition-colors duration-300'>
+		<section className='md:w-1/2 border-4 rounded-xl shadow-lg p-6  animate-pulse'>
+		  <div className='p-6 rounded-lg shadow-lg border-4 transition-colors duration-300'>
 
 			<div className='relative mb-6 aspect-square bg-gray-300 dark:bg-gray-700 rounded-md' />
 
@@ -33,7 +33,7 @@ const LoadingSkeleton = () => {
 		  </div>
 		</section>
 
-		<section className='md:w-1/2 bg-teal shadow-lg p-6 dark:bg-darkerTeal dark:border-white'>
+		<section className='md:w-1/2  shadow-lg p-6  dark:border-white'>
 		  <div>
 			<div className='h-6 bg-gray-300 dark:bg-gray-700 rounded w-1/3 mb-4' />
 			<div className='space-y-3'>
