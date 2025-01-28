82b293cdc430b306471f90d56f3ee4fe3ffc9587
MikeBilbee <cmbilbee@gmail.com>
2025-01-28 14:54:57 -0600

    7.0 Loading Skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
-// import Footer from "./components/Footer";
+import { useState, useEffect } from 'react';
+import Footer from "./Footer";
 import MusicPlayer from "./MusicPlayer";
+import LoadingSkeleton from "./LoadingSkeleton";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  },);
+
   return (
     <div className="h-full flex flex-col justify-between p-8 min-h-screen bg-light-background dark:bg-dark-background">
-      <MusicPlayer />
-      {/* <Footer /> */}
+      {isLoading? (
+        <LoadingSkeleton />
+      ): (
+        <MusicPlayer />
+      )}
+      <Footer />
     </div>
   );
 }
